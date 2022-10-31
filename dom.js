396d7fb32c0c44ce46073e2df5cde5e32d46917b
hilal344 <abdullahhilal344@gmail.com>
2022-10-31 16:55:20 +0530

    task3

---
+++ dom.js
@@ -0,0 +1,9 @@
+let header = document.getElementById("main-header");
+
+header.style.borderBottom = 'solid 3px #000';
+
+let addItems = document.getElementsByTagName('h2');
+//console.log(addItems);
+addItems[0].style.fontWeight = 'bold';
+addItems[0].style.color = 'green';
+
